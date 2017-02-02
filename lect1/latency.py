1bddaf22a1a50c2f98c404f23e3394c1e1583271
Sithu Aung <sithu_aung@intuit.com>
2017-02-01 17:45:28 -0800

    quizz 1

---
+++ lect1/latency.py
@@ -0,0 +1,25 @@
+"""
+Question:
+Pick one IP from each region, find network latency from via the below code snippet
+(ping 3 times), and finally sort regions by the average latency.
+http://ec2-reachability.amazonaws.com/
+Sample output:
+1. us-west-1 [50.18.56.1] - Smallest average latency
+2. xx-xxxx-x [xx.xx.xx.xx] - x
+3. xx-xxxx-x [xx.xx.xx.xx] - x
+...
+15. xx-xxxx-x [xx.xx.xx.xx] - Smallest average latency
+"""
+import subprocess
+
+
+host = "yahoo.com"
+
+ping = subprocess.Popen(
+    ["ping", "-c", "3", host],
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE
+)
+
+out, error = ping.communicate()
+print out
